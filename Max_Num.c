28061aade629a464efa2b4bdb5dd2c26f3e1e967
akshay8281 <akshaypitroda4@gmail.com>
2023-08-16 20:42:50 +0530

    Max-Min-Finder

---
+++ Max_Num.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+ {
+
+int x,y;
+
+printf("Enter the number of x = ");
+scanf("%d",&x);
+
+printf("Enter the number of y = ");
+scanf("%d",&y);
+
+
+if(x>y)
+{
+	printf("The value of x is %d",x);
+}
+
+else
+{
+	printf("The value of y is %d",y);
+}
+
+  return 0;
+}
+
---
+++ Min_Num.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+ {
+
+int A,B;
+
+printf("Enter the number of A = ");
+scanf("%d",&A);
+
+printf("Enter the number of B = ");
+scanf("%d",&B);
+
+
+if(A<B)
+{
+	printf("The value of A is %d",A);
+}
+
+else
+{
+	printf("The value of B is %d",B);
+}
+
+  return 0;
+}
+
